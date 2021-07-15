4205548d6cd2e8118d704c8a0d81cba42a75de9c
Galina-Baeva <galina.baeva@sperasoft.com>
2021-07-15 11:35:18 +0300

    Fourth Task

--- src/FourthTask/DemoException.java
+++ src/FourthTask/DemoException.java
@@ -3,27 +3,29 @@ package FourthTask;
 import java.util.Scanner;
 
 public class DemoException {
-    public static <T> void myMethod(T age) throws Exception {
-        if ((Integer) age < 18) {
+    public static void myMethod(int age) throws Exception {
+        if (age < 18) {
             throw new FirstException("You are under the age");
-        } else if ((Integer) age > 65) {
+
+        } else if (age > 65) {
             throw new SecondException("You'd better be more careful in your age");
-        } else if ((Integer) age >= 18 && (Integer) age <= 65){
-            System.out.println("You are good to go");
+        } else if (age == 40){
+            throw new ThirdException("You can not go at age of 40");
         }
         else {
-            throw new ThirdException("Invalid input of age");
+            System.out.println("You are good to go");
         }
+
     }
 
     public static void main(String[] args) {
         try {
-            //Scanner scanner = new Scanner(System.in);
-            //System.out.println("Enter your age");
-            int n = 200;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your age: ");
+            int n = scanner.nextInt();
             myMethod(n);
         } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
